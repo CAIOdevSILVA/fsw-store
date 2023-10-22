8f46a985764ba41e8b52aa4ca59f2afac0a3b3f3
Caio Silva <caiossantosp5560@gmail.com>
2023-10-22 16:19:32 -0300

    feat: add second banner-home to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -27,8 +27,20 @@ export default async function Home() {
         />
 
         <Categories />
-        <ProductList products={deals}/>
+        
+        <div>
+          <p className='font-bold uppercase pl-5 mb-2'>Ofertas</p>
+          <ProductList products={deals}/>
+        </div>
 
+        <Image 
+          src={'/banner-home-3.png'}
+          width={0}
+          height={0}
+          sizes='100vw'
+          className='w-full h-auto'
+          alt='Até 55% de desconto em mouses!'
+        />
       </div>
     </>
   );
